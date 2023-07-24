0db6941fb65cf42221fe17c11041f630dd7d9e21
Denny Doan <denny-333@hotmail.com>
2023-07-24 13:40:04 -0400

    Added delete functionality to TODO list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import TodoList from './components/ToDoList';
-import NewTodo from './components/NewTodo';
-import { Todo } from './todo.model';
+import TodoList from "./components/ToDoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos(prevTodos => [
+    setTodos((prevTodos) => [
       ...prevTodos,
-      { id: Math.random().toString(), text: text }
+      { id: Math.random().toString(), text: text },
     ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todos => todos.id !== todoId);
+    });
   };
 
   return (
     <div className="App">
       <NewTodo onAddToDo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
